3debdcb3a16ab5f372a39fc36228c6ee6ec8c9b5
kit86095 <69834252+kit86095@users.noreply.github.com>
2020-08-18 11:16:15 +0700

    sent home work by Kit-Papho

---
+++ my601.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void MG(){
+        printf("Kit-Papho\n");
+    
+}
+
+void main(){
+        int b1=10;
+        printf("Hello World\n");
+        printf("%i\n" ,b1);
+        MG();
+}
